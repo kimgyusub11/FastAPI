93791406d650dc21b640ee7d0124ce1ed3a4fe21
name goes here <email goes here>
2025-07-24 13:22:03 +0900

    Edit sql external connection

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import declarative_base
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:kimgyusub!001@localhost/TodoApplicationDatabase"
 
 SQLALCHEMY_DATABASE_URI = "postgresql://todoapplicationdatabase_dofj_user:T2hO0jTApIPdIRtwPvlIYl2AUcKMwaA6@dpg-d20q213ipnbc73di87ug-a.singapore-postgres.render.com/todoapplicationdatabase_dofj"
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(SQLALCHEMY_DATABASE_URI,
+    connect_args={"sslmode": "require"} )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
