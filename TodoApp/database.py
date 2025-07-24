88914390ef99c4f38d310e8b14b7e887c2b9d434
name goes here <email goes here>
2025-07-24 13:17:37 +0900

    Edit sql external connection

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:kimgyusub!001@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:kimgyusub!001@localhost/TodoApplicationDatabase"
+
+SQLALCHEMY_DATABASE_URI = "postgresql://todoapplicationdatabase_dofj_user:T2hO0jTApIPdIRtwPvlIYl2AUcKMwaA6@dpg-d20q213ipnbc73di87ug-a.singapore-postgres.render.com/todoapplicationdatabase_dofj"
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
